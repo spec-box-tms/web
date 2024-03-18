808d40b729aa4c69488dcefa97edda412d2b198b
sr480 <snitkody@gmail.com>
2024-03-18 09:06:58 +0300

    Adds branding

--- src/components/ProjectLayout/ProjectLayout.tsx
+++ src/components/ProjectLayout/ProjectLayout.tsx
@@ -1,6 +1,5 @@
 import * as model from '@/model/pages/project';
 import { cn } from '@bem-react/classname';
-import { Box } from '@gravity-ui/icons';
 import { Icon, PortalProvider, Skeleton } from '@gravity-ui/uikit';
 import { RouteInstance } from 'atomic-router';
 import { useStore } from 'effector-react/scope';
@@ -17,6 +16,7 @@ import { homeRoute, projectRoute, statRoute, testRunsRoute } from '@/model';
 import { InfiniteLoader } from '../InfiniteLoader/InfiniteLoader';
 import { VersionLabel } from '../VersionLabel/VersionLabel';
 import './ProjectLayout.css';
+import { Logo } from '../Logo/Logo';
 
 const bem = cn('ProjectLayout');
 
@@ -92,7 +92,7 @@ export const ProjectLayout: FC<ProjectLayoutProps> = (props) => {
                 size="l"
                 pin="circle-circle"
               >
-                <Icon size={24} data={Box} />
+                <Icon size={34} data={Logo} />
               </RouteLinkButton>
             </div>
             <div className={bem('Title')}>
